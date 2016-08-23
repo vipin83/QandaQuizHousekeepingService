e5c977c70151d5abbc1a0a9ed46328e76f8dfdcb
vipin83 <vipin.agarwal83@gmail.com>
2016-08-23 14:23:30 +0100

    Initial migration from TFS to Git source control
    Initial migration from TFS to Git source control

---
+++ QandaQuizHousekeepingService/Housekeeping.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ef = QandaQuizEntityFramework;
+
+namespace QandaQuizHousekeepingService
+{
+    public static class Housekeeping
+    {
+
+        public static ef.QandaQuizContext dbContext = new ef.QandaQuizContext();
+        
+        public static void runMundaneTasks()
+        {
+            //run this peice of code in a timely fashion
+
+            CreateLogEntry("Start");
+          
+            var currentActiveQuizDetails = dbContext.quizDetails
+                                                    .Where(qd => !qd.quizLapsed && qd.quizCurrentlyActive)
+                                                    .FirstOrDefault();
+
+
+            //1. check if active date time of current quiz is past the next quiz active date time. if so then lapse first quiz and make next quiz state as active
+            if (currentActiveQuizDetails != null)
+            {
+
+                CreateLogEntry("currently active quiz id# " + currentActiveQuizDetails.Id + " active from " + currentActiveQuizDetails.quizAvtiveDateTime);
+                
+                ef.QuizDetail nextToBeActiveQuizDetails = dbContext.quizDetails
+                                                           .Where(qd => !qd.quizLapsed && qd.quizAvtiveDateTime > currentActiveQuizDetails.quizAvtiveDateTime)
+                                                           .OrderBy(o => o.quizAvtiveDateTime)
+                                                           .FirstOrDefault();
+
+
+                if (nextToBeActiveQuizDetails != null) //expire current quiz only if there is any other quiz in the system which can be made active
+                {
+                    CreateLogEntry("next to-be active quiz id# " + nextToBeActiveQuizDetails.Id + " active from " + nextToBeActiveQuizDetails.quizAvtiveDateTime);
+
+                    //check if current time is past the next ToBe active quiz datetime
+                    if (DateTime.Now >= nextToBeActiveQuizDetails.quizAvtiveDateTime)
+                    {
+                        CreateLogEntry("expiring current quiz");
+
+                        currentActiveQuizDetails.quizLapsed = true;
+                        currentActiveQuizDetails.quizCurrentlyActive = false;
+
+                        nextToBeActiveQuizDetails.quizLapsed = false;
+                        nextToBeActiveQuizDetails.quizCurrentlyActive = true;
+                        
+                        dbContext.SaveChanges();
+                    }
+                }
+            }
+
+            //2. check if there is any lapsed quiz which does not have winner
+            var lapsedQuizNoWinner = dbContext.quizDetails
+                                              .Where(qd => qd.quizLapsed && String.IsNullOrEmpty(qd.quizWinnerId))
+                                              .OrderBy(o => o.quizAvtiveDateTime)
+                                              .ToList();
+
+            foreach (var lapsedQuiz in lapsedQuizNoWinner)
+            {
+
+                var correctEntriesForQuiz = dbContext.quizPlayDetails
+                                                     .Where(p => p.QuizDetail.Id == lapsedQuiz.Id &&
+                                                                 p.QuizAnswer.Id == p.QuizDetail.QuizQuestion.QuizAnswers.Where(q => q.quizAnswerCorrect).FirstOrDefault().Id &&
+                                                                 !p.IsThisFreePlay
+                                                           );
+
+                var correctEntriesCountForQuiz = correctEntriesForQuiz.Count();
+
+                //(announce winner only if it has receievd sufficient 'correct' responses? )
+                if (correctEntriesCountForQuiz >= lapsedQuiz.quizTimesNumberOfEntriesAllowed)
+                {
+                    //3. announce winner
+                    var quizWinnerNumber = lapsedQuiz.quizWinnerNumber;
+
+                    //sort quiz answer entries in the order they were answered
+                    var quizWinnerEntry = correctEntriesForQuiz.OrderBy(o => o.quizSubmittedDateTime)
+                                                               .Skip(quizWinnerNumber - 1)
+                                                               .FirstOrDefault();
+
+                    //update lapsed quiz with the winner number
+                    lapsedQuiz.quizWinnerId = quizWinnerEntry.user_Id;
+                    dbContext.SaveChanges();
+
+                    //4. Send email to winner + site owner
+                    var emailMgr = new clsEmail();
+
+                    try
+                    {
+                        emailMgr.SendEmailUsingSMTP(quizWinnerEntry.AspNetUser.Email,
+                            "admin@QandAQuiz.com",
+                            "Congratulations! you are the winner of QandA Quiz!!",
+                            "You have won the quiz with prize money of " + lapsedQuiz.quizPrizeMoney.ToString("C"));
+
+                        emailMgr.SendEmailUsingSMTP("admin@QandAQuiz.com",
+                            "admin@QandAQuiz.com",
+                            "Quiz # " + lapsedQuiz.Id + "(" + lapsedQuiz.quizTitle + ") winner announced",
+                            quizWinnerEntry.AspNetUser.Email + " has been declared winner of quiz# " + lapsedQuiz.Id + "(" + lapsedQuiz.quizTitle + ")");
+                    }
+                    catch
+                    { }
+                }
+
+            }
+
+            dbContext.LogEntries.Add(new ef.LogEntry
+            {
+                LogEntryDateTime = DateTime.Now,
+                LogEntryDescription = "Housekeeping Service - Housekeeping - End"
+            });
+
+            dbContext.SaveChanges();
+        }
+        
+        private static void CreateLogEntry(string message)
+        {
+
+            dbContext.LogEntries.Add(new ef.LogEntry
+            {
+                LogEntryDateTime = DateTime.Now,
+                LogEntryDescription = "Housekeeping Service - " + message
+            });
+
+            dbContext.SaveChanges();
+
+        }
+
+    }
+}
---
+++ QandaQuizHousekeepingService/Service1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace QandaQuizHousekeepingService
+{
+    public partial class Service1 : ServiceBase
+    {
+        private Timer quizElapsedCheckTimer = null;
+
+        private bool alreadyStarted = false;                
+        private int lastMinCalled = -1;
+
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            quizElapsedCheckTimer = new Timer();
+            this.quizElapsedCheckTimer.Enabled = true;
+            this.quizElapsedCheckTimer.Interval = 2000;
+            this.quizElapsedCheckTimer.Elapsed += new ElapsedEventHandler(timer_tick);
+            this.quizElapsedCheckTimer.Start();
+        }
+
+        private void timer_tick(object sender, ElapsedEventArgs e)
+        {
+            int iMinute = -1;            
+
+            if (!alreadyStarted)
+            {                
+                //check if its 15 min past of an hour 
+                iMinute = DateTime.Now.Minute;
+                
+                if ((iMinute == 15 || iMinute == 30 || iMinute == 45 || iMinute == 00) && (iMinute != lastMinCalled))
+                {
+                    alreadyStarted = true;
+                    
+                    QandaQuizHousekeepingService.Housekeeping.runMundaneTasks();
+
+                    lastMinCalled = DateTime.Now.Minute;
+                    alreadyStarted = false;
+                }
+            }
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
